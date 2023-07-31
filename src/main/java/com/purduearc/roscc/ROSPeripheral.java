49f08a93c4c1d9d9f63369a42cd3c982970933a2
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2023-07-30 23:44:32 -0500

    ROSPeripheral Lua function skeletons

--- src/main/java/com/purduearc/roscc/ROSPeripheral.java
+++ src/main/java/com/purduearc/roscc/ROSPeripheral.java
@@ -2,6 +2,7 @@ package com.purduearc.roscc;
 
 import com.purduearc.roscc.blocks.ROSPeripheralBlockEntity;
 
+import dan200.computercraft.api.lua.LuaFunction;
 import dan200.computercraft.api.peripheral.IComputerAccess;
 import dan200.computercraft.api.peripheral.IPeripheral;
 
@@ -21,6 +22,16 @@ public class ROSPeripheral implements IPeripheral {
 	@Override
 	public boolean equals(IPeripheral other) {
 		return this == other || (other instanceof ROSPeripheral && ((ROSPeripheral) other).blockEntity == blockEntity);
+	}
+	
+	@LuaFunction
+	public final void startNode(IComputerAccess computer) {
+		this.active = true;
+	}
+	
+	@LuaFunction
+	public final void stopNode(IComputerAccess computer) {
+		this.active = false;
 	}
 	
 	@Override
