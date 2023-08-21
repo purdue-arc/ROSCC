7b8541ba3c2b886ffe0fcd62a77836efd6b31ecb
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2023-08-21 15:18:03 -0400

    Add blocks, item nbt, block nbt

--- src/main/java/com/purduearc/roscc/server/CHandlerStatus.java
+++ src/main/java/com/purduearc/roscc/server/CHandlerStatus.java
@@ -2,12 +2,21 @@ package com.purduearc.roscc.server;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Vec3i;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class CHandlerStatus implements HttpHandler {
 	
@@ -35,9 +44,12 @@ public class CHandlerStatus implements HttpHandler {
 		int requestedEntitiesRadius = Integer.parseInt(query.getOrDefault("entities", "0"));
 		boolean requestedInventory = query.containsKey("inv");
 		boolean requestedPosition = query.containsKey("pos");
+		boolean requestedBlockNBT = query.containsKey("blocknbt");
+		boolean requestedItemNBT = query.containsKey("itemnbt");
 		TurtleAccessWrap turtle = ROSCCServer.turtles.get(id);
 		SimpleItem[] inv = null;
 		SimplePos pos = null;
+		SimpleBlock[] blocks = null;
 		if (turtle == null) {
 			response += "400 Bad Request (Invalid ID)";
 			exchange.sendResponseHeaders(400, response.length());
@@ -47,16 +59,30 @@ public class CHandlerStatus implements HttpHandler {
 	        return;
 		}
 		if (requestedInventory) {
-			inv = SimpleItem.toSimpleItems(turtle.turtle.getInventory());
+			inv = new SimpleItem[turtle.turtle.getInventory().getContainerSize()];
+			for (int i = 0; i < inv.length; i++) {
+				inv[i] = new SimpleItem(turtle.turtle.getInventory().getItem(i), requestedItemNBT);
+			}
 		}
 		if (requestedPosition) {
 			pos = new SimplePos(turtle.turtle.getPosition());
 		}
-		response = turtleStatusAdapter.toJson(new TurtleStatus(pos, inv));
+		if (requestedBlocksRadius > 0) {
+			Vec3i cubeCorner = new Vec3i(requestedBlocksRadius, requestedBlocksRadius, requestedBlocksRadius);
+			ArrayList<SimpleBlock> blocksList = new ArrayList<SimpleBlock>();
+			for (BlockPos bp : BlockPos.betweenClosed(turtle.turtle.getPosition().subtract(cubeCorner), turtle.turtle.getPosition().offset(cubeCorner))) {
+				String nbtString = null;
+				if (requestedBlockNBT) {
+					CompoundTag nbt = NbtUtils.writeBlockState(turtle.turtle.getLevel().getBlockState(bp));
+					nbt.remove("Name");
+					nbtString = nbt.toString().equals("{}") ? null : nbt.toString();
+				}
+				blocksList.add(new SimpleBlock(bp.getX(), bp.getY(), bp.getZ(), ForgeRegistries.BLOCKS.getKey(turtle.turtle.getLevel().getBlockState(bp).getBlock()).toString(), nbtString));
+			}
+			blocks = blocksList.toArray(new SimpleBlock[blocksList.size()]);
+		}
 		
-		
-		// localhost:8080/status?id=1
-		
+		response = turtleStatusAdapter.toJson(new TurtleStatus(pos, inv, blocks));
         exchange.sendResponseHeaders(200, response.length());
         OutputStream os = exchange.getResponseBody();
         os.write(response.getBytes());
