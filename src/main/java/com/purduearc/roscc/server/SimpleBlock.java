e612a847a9fa90a12d286702766a181eb696da62
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2023-08-21 15:17:51 -0400

    Add SimpleBlock

---
+++ src/main/java/com/purduearc/roscc/server/SimpleBlock.java
@@ -0,0 +1,17 @@
+package com.purduearc.roscc.server;
+
+public class SimpleBlock {
+	public final int x;
+	public final int y;
+	public final int z;
+	public final String id;
+	public final String nbt;
+	public SimpleBlock(int x, int y, int z, String id, String nbt) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.id = id;
+		this.nbt = nbt;
+	}
+	
+}
