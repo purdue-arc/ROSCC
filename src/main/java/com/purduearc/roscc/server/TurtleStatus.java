b9090ad1148f254ddc9928f6627274999e5cb8b8
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2023-08-21 15:17:44 -0400

    Add blocks to TurtleStatus

--- src/main/java/com/purduearc/roscc/server/TurtleStatus.java
+++ src/main/java/com/purduearc/roscc/server/TurtleStatus.java
@@ -3,8 +3,10 @@ package com.purduearc.roscc.server;
 public class TurtleStatus {
 	public final SimplePos pos;
 	public final SimpleItem[] inv;
-	public TurtleStatus(SimplePos pos, SimpleItem[] inv) {
+	public final SimpleBlock[] blocks;
+	public TurtleStatus(SimplePos pos, SimpleItem[] inv, SimpleBlock[] blocks) {
 		this.pos = pos;
 		this.inv = inv;
+		this.blocks = blocks;
 	}
 }
