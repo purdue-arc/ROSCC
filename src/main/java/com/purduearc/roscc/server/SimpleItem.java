37f3cb9c680d7127a99eed4e52636c338bc5305c
jcrm1 <52137472+jcrm1@users.noreply.github.com>
2023-08-21 15:17:25 -0400

    Remove SimpleItem.toSimpleItems; int id->String id
    Reasoning: Only used once

--- src/main/java/com/purduearc/roscc/server/SimpleItem.java
+++ src/main/java/com/purduearc/roscc/server/SimpleItem.java
@@ -1,29 +1,25 @@
 package com.purduearc.roscc.server;
 
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.Container;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class SimpleItem {
-	public final int id;
+	public final String id;
 	public final int count;
 	public final String nbt;
-	public SimpleItem(int id, int count, String nbt) {
+	public SimpleItem(String id, int count, String nbt) {
 		this.id = id;
 		this.count = count;
 		this.nbt = nbt;
 	}
-	public SimpleItem(ItemStack item) {
-		this.id = Item.getId(item.getItem());
+	public SimpleItem(ItemStack item, boolean includeNBT) {
+		this.id = ForgeRegistries.ITEMS.getKey(item.getItem()).toString();
 		this.count = item.getCount();
-		String fullNBT = item.serializeNBT().toString();
-		this.nbt = fullNBT.equals("{Count:1b,id:\"minecraft:air\"}") ? null : fullNBT;
-	}
-	public static SimpleItem[] toSimpleItems(Container container) {
-		SimpleItem[] items = new SimpleItem[container.getContainerSize()];
-		for (int i = 0; i < items.length; i++) {
-			items[i] = new SimpleItem(container.getItem(i));
-		}
-		return items;
+		CompoundTag fullNBT = item.serializeNBT();
+		fullNBT.remove("Count");
+		fullNBT.remove("id");
+		this.nbt = !includeNBT || fullNBT.toString().equals("{}") ? null : fullNBT.toString();
 	}
 }
